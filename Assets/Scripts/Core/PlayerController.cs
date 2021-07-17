0021bcb618a0c71108ac846c9095d6fd094d2ccd
DESKTOP-CN6KRA6\esyun <tmddbs0602@korea.ac.kr>
2021-07-17 16:48:41 +0900

    부활 시스템 틀 구현

--- Assets/Scripts/Core/PlayerController.cs
+++ Assets/Scripts/Core/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -46,7 +47,11 @@ public class PlayerController : MonoBehaviour
 
     void Revive()
     {
-        
+        string data = SaveManager.instance.popData();
+        if (data != null)
+        {
+            SceneManager.LoadScene(data);
+        }
     }
 
 }
