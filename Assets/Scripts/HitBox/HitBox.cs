678eab46b50027518a59f9221414b4ceeaafd215
kudave20 <dave1315@korea.ac.kr>
2021-08-04 22:30:01 +0900

    닌자 구현

--- Assets/Scripts/HitBox/HitBox.cs
+++ Assets/Scripts/HitBox/HitBox.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class HitBox : MonoBehaviour
 {
-    // Start is called before the first frame update
     public int dmg;
-    void Start()
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-
+        if (!collision.gameObject.tag.Contains("Enemy"))
+        {
+            gameObject.SetActive(false);
+        }
     }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
 }
