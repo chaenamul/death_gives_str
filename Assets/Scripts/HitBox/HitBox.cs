d08faa52e26df02590042f936fce28a400d478c0
DESKTOP-KHL63IV\jjun0403 <jjun2205@gmail.com>
2021-08-08 22:21:03 +0900

    피격시 캐릭터 무적, 깜빡깜빡 임시 구현 (레이어 X, bool 변수로 구현)

--- Assets/Scripts/HitBox/HitBox.cs
+++ Assets/Scripts/HitBox/HitBox.cs
@@ -7,15 +7,20 @@ public class HitBox : MonoBehaviour
 {
     public int dmg;
     public object subject;
+    private int countTime = 0;
+    private bool inv = false;
     protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
         print(subject as PlayerController);
         print("collision: " + collision.gameObject);
 
 
-        if(subject as Enemy && collision.tag == "Player") // Enemy -> Player 공격
+        if(subject as Enemy && collision.tag == "Player"&& !inv ) // Enemy -> Player 공격
         {
             GameManager.instance.playerController.GetDmg((Enemy)subject, dmg);
+            inv = true;
+            InvokeRepeating("light", 0f, 0.2f);
+            Invoke("stoplight", 1f);
         }
         else if(subject as PlayerController && collision.tag.Contains("Enemy"))
         {
@@ -24,13 +29,33 @@ public class HitBox : MonoBehaviour
             collision.GetComponent<Enemy>().GetDmg(dmg);
         }
     }
+    
     void Shaking()
     {
         Camera.main.transform.position = Random.insideUnitSphere * 0.05f + new Vector3(0f, 0f, -10f);
     }
+    
     void StopShaking()
     {
         CancelInvoke("Shaking");
         Camera.main.transform.position = new Vector3(0f, 0f, -10f);
     }
+    
+    void light()
+    {
+        if (countTime % 2 == 0)
+            GameObject.Find("Player").GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 90);
+        else
+            GameObject.Find("Player").GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 180);
+        countTime++;
+    }
+    void stoplight()
+    {
+        CancelInvoke("light");
+        GameObject.Find("Player").GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+        countTime = 0;
+        inv = false;
+    }
+
+  
 }
