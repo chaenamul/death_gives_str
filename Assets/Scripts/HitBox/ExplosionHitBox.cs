fcc2fc811c78b9b7969e2104f4e12e2ffa738477
DESKTOP-CN6KRA6\esyun <tmddbs0602@korea.ac.kr>
2021-09-04 17:49:04 +0900

    null 참조 오류 수정

--- Assets/Scripts/HitBox/ExplosionHitBox.cs
+++ Assets/Scripts/HitBox/ExplosionHitBox.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ExplosionHitBox : HitBox
 {
@@ -21,10 +22,11 @@ public class ExplosionHitBox : HitBox
     }
     IEnumerator Explosion(Vector3 pos)
     {
-        explosion.gameObject.transform.position = pos;
-        explosion.gameObject.SetActive(true);
+        if(explosion.gameObject)
+            explosion.gameObject.transform.position = pos;
+        explosion?.gameObject?.SetActive(true);
         yield return new WaitForSeconds(0.3f);
-        explosion.gameObject.SetActive(false);
+        explosion?.gameObject?.SetActive(false);
         
     }
 }
